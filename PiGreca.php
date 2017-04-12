4b270447eea7a263325ffd4ee97b2175d14321ed
AndreaZaccagno <januaryzack90@gmail.com>
2017-04-12 11:09:18 +0200

    Update PiGreca.php

--- PiGreca.php
+++ PiGreca.php
@@ -4,9 +4,13 @@ print "Hello world! \n Questa è una mia amica greca, si chiama Pi: ".pi()."! \n
 
 // Modifica successiva
 
-// Modifica da Folder
+// Modifica 1
 
-// Modifica da Web
+$ciao = "ehilà"
+for($i = 0; $i < strlen($ciao); $i++)
+{
+	print substr($ciao, $i, 1)."\n";	
+}
 
 
 ?>
